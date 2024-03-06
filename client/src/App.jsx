df96cc994d0123a416999e6295b119ed0a1ee056
jaydevariya2027 <jaydevariya2027@gmail.com>
2024-03-06 11:33:47 +0530

    fixed bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div></div>
   );
 }
