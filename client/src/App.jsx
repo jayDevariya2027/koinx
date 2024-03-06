777e914ff16e0ac9398550f0c3c40e5c219e8244
jaydevariya2027 <jaydevariya2027@gmail.com>
2024-03-06 11:30:07 +0530

    create home page and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
