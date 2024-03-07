4fadcc63aa068c8a1c98938de5b7f51388c72d22
jaydevariya2027 <jaydevariya2027@gmail.com>
2024-03-07 23:18:05 +0530

    fixed some bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <div>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
+      <Home />
       <Footer />
-    </BrowserRouter>
+    </div>
   );
 }
