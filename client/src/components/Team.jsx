396fb5ad192039120ef4fc5f6eb89d23474fcb46
jaydevariya2027 <jaydevariya2027@gmail.com>
2024-03-07 19:09:31 +0530

    completed team component

--- client/src/components/Team.jsx
+++ client/src/components/Team.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
 
 export default function Team() {
   return (
     <div>
-      
+      <p className="text-3xl font-semibold">Team</p>
+      <p className="mt-6 text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
+        voluptatum beatae praesentium molestiae ullam pariatur repudiandae eius
+        non inventore a sit sed, repellendus rerum nihil facilis esse. Possimus,
+        nulla nobis!
+      </p>
+
+      <div
+        className="p-3 flex border-white rounded-md mt-6 justify-center"
+        style={{ background: "#E8F4FD" }}
+      >
+        <div className="w-3/5 p-2 flex flex-col items-center justify-center">
+          <img
+            className="h-24 w-24 border-0 rounded-md"
+            src="../src/assets/image1.jpg"
+            alt="bitcoin"
+          />
+          <p className="font-semibold mt-3 text-center">John Smith</p>
+          <p className="text-sm">Designation here</p>
+        </div>
+
+        <div className="mr-1 ml-1 mt-2 flex flex-col justify-center items-center p-2">
+          <p className="text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
+            cum neque quo aliquam maiores delectus iusto et itaque autem
+            corrupti adipisci repudiandae corporis, explicabo ea minus quasi!
+            Obcaecati, fuga quae tempore quos odio rem aliquid molestiae laborum
+          </p>
+        </div>
+      </div>
+      <div
+        className="p-3 flex border-white rounded-md mt-6 justify-center"
+        style={{ background: "#E8F4FD" }}
+      >
+        <div className="w-3/5 p-2 flex flex-col items-center justify-center">
+          <img
+            className="h-24 w-24 border-0 rounded-md"
+            src="../src/assets/image1.jpg"
+            alt="bitcoin"
+          />
+          <p className="font-semibold mt-3 text-center">John Smith</p>
+          <p className="text-sm">Designation here</p>
+        </div>
+
+        <div className="mr-1 ml-1 mt-2 flex flex-col justify-center items-center p-2">
+          <p className="text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
+            cum neque quo aliquam maiores delectus iusto et itaque autem
+            corrupti adipisci repudiandae corporis, explicabo ea minus quasi!
+            Obcaecati, fuga quae tempore quos odio rem aliquid molestiae laborum
+          </p>
+        </div>
+      </div>
+      <div
+        className="p-3 flex border-white rounded-md mt-6 justify-center"
+        style={{ background: "#E8F4FD" }}
+      >
+        <div className="w-3/5 p-2 flex flex-col items-center justify-center">
+          <img
+            className="h-24 w-24 border-0 rounded-md"
+            src="../src/assets/image1.jpg"
+            alt="bitcoin"
+          />
+          <p className="font-semibold mt-3 text-center">John Smith</p>
+          <p className="text-sm">Designation here</p>
+        </div>
+
+        <div className="mr-1 ml-1 mt-2 flex flex-col justify-center items-center p-2">
+          <p className="text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
+            cum neque quo aliquam maiores delectus iusto et itaque autem
+            corrupti adipisci repudiandae corporis, explicabo ea minus quasi!
+            Obcaecati, fuga quae tempore quos odio rem aliquid molestiae laborum
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
